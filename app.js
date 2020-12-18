4168fd39ad58bcf8f5c1c0b52ee3aa6cb3de4297
muki wu <mukispace@gmail.com>
2020-12-18 17:19:56 +0800

    新增跨網域

--- app.js
+++ app.js
@@ -17,6 +17,20 @@ app.set('view engine', 'ejs');
 // 連接數據庫
 mongoose.connect(`mongodb://localhost:27017/test`);
 
+// 跨網域
+app.all('/*', function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
