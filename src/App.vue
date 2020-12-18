0d4cd383d5669b8e7cf35df83cff4dbf985ba7d9
muki wu <mukispace@gmail.com>
2020-12-18 15:57:12 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<div id="app">
+  <nav>
+    <div class="container">
+      <ul class="nav__left">
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/admin">Admin</router-link>
+        </li>
+        <li>
+          <router-link to="/cart">Cart</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <router-view />
+</div>
 </template>
 
 <script>
---
+++ src/pages/admin/Products.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <div class="title">
+      <h1>This is Product</h1>
+    </div>
+  </div>
+</template>
